b3ec1220ce547c6370a19fe817f7a275aaeabfff
FOCKUSTY <wwddwwwd@mail.ru>
2026-07-09 14:56:52 +0500

    fixes

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 export default async (req, res) => {
-  const { reqHandler } = await import('../dist/src/server/server.mjs');
+  const { reqHandler } = await import('../dist/anniversary-2026/server/server.mjs');
   return reqHandler(req, res);
 };
